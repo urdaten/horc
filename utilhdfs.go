20ef85823c3f19ade5c69cd6a92dc789e08a751e
sharop <sharop@gmail.com>
2020-06-26 18:42:02 -0500

    Path changes
    TODO: Make path verification

--- utilhdfs.go
+++ utilhdfs.go
@@ -20,12 +20,11 @@ type Settings struct {
 	}
 }
 
-
-type DataPack struct{
-	p hdfs.FileReader
+type DataPack struct {
+	p      hdfs.FileReader
 	offset int64
-	size int64
-	fInfo os.FileInfo
+	size   int64
+	fInfo  os.FileInfo
 	client *hdfs.Client
 }
 
@@ -33,32 +32,35 @@ func (t DataPack) Size() int64 {
 	return t.fInfo.Size()
 }
 
-func (t DataPack) ReadAt(p []byte, off int64)(n int, err error){
-	fileName := PathHdfs +t.fInfo.Name()
+func (t DataPack) ReadAt(p []byte, off int64) (n int, err error) {
+	fileName := PathHdfs + t.fInfo.Name()
 	file, err := t.client.Open(fileName)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	n, err = file.ReadAt(p, off)
-	return n,err
+	return n, err
 }
+
 var PathHdfs string = ""
 
-func NewReadAtUn(client *hdfs.Client,  offset int64, fileInfo os.FileInfo) orc.SizedReaderAt {
-	return DataPack{client:client, offset: offset, fInfo: fileInfo }
+func NewReadAtUn(client *hdfs.Client, offset int64, fileInfo os.FileInfo) orc.SizedReaderAt {
+	return DataPack{client: client, offset: offset, fInfo: fileInfo}
 }
+
 const fileName = "conf.yaml"
-func main(){
+
+func main() {
 
 	yamlFile, err := ioutil.ReadFile(fileName)
-	if !(err==nil){
+	if !(err == nil) {
 		panic(err)
 	}
 
 	cFile := Settings{}
 	err = yaml.Unmarshal([]byte(yamlFile), &cFile)
-	if !(err==nil){
+	if !(err == nil) {
 		panic(err)
 	}
 
@@ -71,7 +73,6 @@ func main(){
 	// Create new client to HDFS
 	client, _ := hdfs.New(cFile.Cluster.Address[0] /*"localhost:8020"*/)
 
-
 	// Red HDFS directory
 	fInfos, err := client.ReadDir(PathHdfs)
 	if !(err == nil) {
@@ -79,25 +80,24 @@ func main(){
 	}
 
 	// Iterate files from directory
-	for _, finFo := range fInfos{
+	for _, finFo := range fInfos {
 
 		// read only files
-		if finFo.IsDir()!= true && strings.Contains(finFo.Name(),".orc")  {
+		if finFo.IsDir() != true && strings.Contains(finFo.Name(), ".orc") {
 
 			// Print name and size of the file.
-			fmt.Println( finFo.Name(), finFo.Size())
+			fmt.Println(finFo.Name(), finFo.Size())
 
 			data := NewReadAtUn(client, 0, finFo)
 
-
-			orcOps,_ := orc.NewReader(data)
+			orcOps, _ := orc.NewReader(data)
 
 			orcOps.Schema().Columns()
 
-			c:=orcOps.Select(orcOps.Schema().Columns()...)//"col_1" , "col_2", "col_3")
+			c := orcOps.Select(orcOps.Schema().Columns()...) //"col_1" , "col_2", "col_3")
 
-			for c.Stripes(){
-				for c.Next(){
+			for c.Stripes() {
+				for c.Next() {
 					log.Println(c.Row())
 				}
 			}
